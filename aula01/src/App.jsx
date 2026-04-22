aea8fce1a2edc4e36b4d42e9c54e445da64a5a55
joaofrancisco212 <joaof211209@gmail.com>
2026-04-22 14:58:48 -0300

    22/04

---
+++ aula01/src/App.jsx
@@ -0,0 +1,24 @@
+ import "./App.css";
+
+export default function App()
+{
+  let nome, idade, dias;
+  nome = "Joao Francisco";
+  idade = 47;
+  dias = idade = * 365
+  return (
+    <div>
+
+      <h1>Olá Mundo !!!</h1>
+
+      <div className="conteudo">
+
+        <p>Olá, seja bem vindo ao React com JS.</p>
+        <p>O aluno {nome} já viveu {dias}</p>
+      
+
+      </div>
+
+    </div>
+  )
+}
