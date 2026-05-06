606254c0c9f03482fa94d1982d0d75dc4d292a69
joaofrancisco212 <joaof211209@gmail.com>
2026-05-06 14:43:08 -0300

    06/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -6,7 +6,10 @@ export default function App()
      <div>
         <h1>Aula 02 - Estudo de Componentes e Props</h1>
 
+         <div className="card">
+            <h3>Chamada para o Exemplo 1</h3> 
 
+         </div>
 
      </div>
 
