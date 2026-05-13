3b5eeebdda52fe580a9db8b532c9a74d81d83886
joaofrancisco212 <joaof211209@gmail.com>
2026-05-13 14:58:31 -0300

    13/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import Exemplo1 from "./paginas/Exemplo1";
+import Exemplo2 from "./paginas/Exemplo2";
+import Exercicio1 from "./paginas/Exercicio1";
+
 
 export default function App()
 {
@@ -8,10 +12,31 @@ export default function App()
 
          <div className="card">
             <h3>Chamada para o Exemplo 1</h3> 
+            <Exemplo1 numero={33} />  
+            <Exemplo1 numero={2} />  
+            <Exemplo1 numero={22} /> 
+            <Exemplo1 numero={432} /> 
+            <Exemplo1 numero={673} />    
+           </div>
 
-         </div>
+           <div className="card">
+            <h3>Chamada para Exemplo 2</h3>
+            <Exemplo2 numero1={33} numero2={34} />
+            <Exemplo2 numero1={42} numero2={-345} />
+            <Exemplo2 numero1={4.5} numero2={6.8} />
+           </div>
+
+           <div className="card">
+            <h3>Camadas para o componente Exercicio 1</h3>
+
+            <Exercicio1 fahrenheit={100} />
+               <hr />
+            <Exercicio1 fahrenheit={70} />
+
+           </div>
 
      </div>
+
 
   )
 }
--- aula02/src/paginas/Exercicio1.jsx
+++ aula02/src/paginas/Exercicio1.jsx
@@ -1,8 +1,11 @@
-export default function Exercicio1()
+export default function Exercicio1( {fahrenheit} )
 {
+    let celsius;
+    celsius = (fahrenheit - 32) * 5 /9;
+
     return (
         <div>
-            
+          celsius é igual a {celsius}
         </div>
     )
 }
